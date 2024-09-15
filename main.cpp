e3be296f26fe7e2410f768b098444987c37a48f5
ArtaTwin <artail1803@gmail.com>
2024-09-16 01:56:02 +0200

    update v0.0

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+
+    return 1;
+}
+
+ git config --global user.email "artail1803@gmail.com"
+ git config --global user.name "ArtaTwin"
