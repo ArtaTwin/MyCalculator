38b36797fd9f2f2c9df4e45e2e2b3a3b5d61d055
ArtaTwin <artail1803@gmail.com>
2024-09-18 16:07:37 +0200

    update v5.1

--- main.cpp
+++ main.cpp
@@ -2,21 +2,39 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
     int ReturnCode = 0;
 
     float Dividend = 1;
     cout << "Dividend: " << endl;
     cin >> Dividend;
 
-    if (!cin.fail()) {
+    if (!cin.fail())
+    {
         float Divisor = 1;
         cout << "Divisor: " << endl;
         cin >> Divisor;
 
-        float Result = (Dividend/Divisor);
-        cout << Result << endl;
-    }else{
+        if (!cin.fail())
+        {
+            float Result = (Dividend/Divisor);
+            cout << Result << endl;
+        }
+        else
+        {
+            cerr << "Input error, not a number?" << endl;
+            
+            cin.clear();
+
+            char BadInput[5];
+            cin >> BadInput;
+
+            ReturnCode = 1;
+        };
+    }
+    else
+    {
         cerr << "Input error, not a number?" << endl;
 
         cin.clear();
@@ -25,7 +43,7 @@ int main(int argc, char* argv[]){
         cin >> BadInput;
 
         ReturnCode = 1;
-    }
+    };
 
 
     char StopCharacter;
