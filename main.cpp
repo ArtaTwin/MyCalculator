a47d7667c7c26965929fcf0965b63993efc0ed94
ArtaTwin <artail1803@gmail.com>
2024-09-18 15:57:50 +0200

    update v5.0

--- main.cpp
+++ main.cpp
@@ -3,20 +3,34 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+    int ReturnCode = 0;
+
     float Dividend = 1;
     cout << "Dividend: " << endl;
     cin >> Dividend;
 
-    float Divisor = 1;
-    cout << "Divisor: " << endl;
-    cin >> Divisor;
+    if (!cin.fail()) {
+        float Divisor = 1;
+        cout << "Divisor: " << endl;
+        cin >> Divisor;
 
-    float Result = (Dividend/Divisor);
-    cout << Result << endl;
+        float Result = (Dividend/Divisor);
+        cout << Result << endl;
+    }else{
+        cerr << "Input error, not a number?" << endl;
+
+        cin.clear();
+
+        char BadInput[5];
+        cin >> BadInput;
+
+        ReturnCode = 1;
+    }
+
 
     char StopCharacter;
     cout << endl << "Press a key and \"Enter\": ";
     cin >> StopCharacter;
 
-    return 0;
+    return ReturnCode;
 }
